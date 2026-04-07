3a08f9907b66887f327f3384990732b1630cd224
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-04-07 11:07:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+	event.preventDefault();
 	let r = document.getElementById('radius');
 	let v = document.getElementById('volume');
 	let radius = parseInt(r.value);
-	console.log(radius);
 	if(radius > 0){
-		console.log("button clicked")
 		let volume = (4/3) * (Math.PI * radius**3);
 		v.value = volume;
-		return v.value;
 	}
 	else{
-		console.log("else");
 		v.value  = NaN;
-		return v.value;
 	}
 } 
 
