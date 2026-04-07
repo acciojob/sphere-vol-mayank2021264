d446ec9614b283807b6430464665468065a31624
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-04-07 10:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function volume_sphere() {
     //Write your code here
-  
+	let r = document.getElementById('radius');
+	let v = document.getElementById('volume');
+	let radius = parseInt(r.value);
+	console.log(radius);
+	if(radius > 0){
+		console.log("button clicked")
+		let volume = (4/3) * (Math.PI * radius**3);
+		v.value = volume;
+		return v.value;
+	}
+	else{
+		console.log("else");
+		v.value  = NaN;
+		return v.value;
+	}
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+	document.getElementById('MyForm').onsubmit
+		= volume_sphere;
+}
